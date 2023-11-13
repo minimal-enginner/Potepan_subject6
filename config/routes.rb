75589d94332bb427ed7f294fb290351c0363cc85
菊川裕平 <yuh216@macbook-air.tailca2b4.ts.net>
2023-11-13 10:17:35 +0900

    routes.rbのget重複分修正及びroot設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
+  # get 'tasks/index'
+  # get 'tasks/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
+  root to: 'tasks#index'
 end
